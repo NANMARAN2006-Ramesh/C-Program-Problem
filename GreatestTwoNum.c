497e417da4b156ef9c2866fcc3542ff38e367f26
NANMARAN2006-Ramesh <nmaran294@gmail.com>
2026-01-26 10:18:06 +0530

    Greatest of two number

---
+++ GreatestTwoNum.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int num1,num2;
+    scanf("%d",&num,&num2);
+    if(num>num2){
+        printf("%d is greater",num1);
+    }
+    else{
+        printf("%d is greater",num2);
+    }
+
+    return 0;
+}
