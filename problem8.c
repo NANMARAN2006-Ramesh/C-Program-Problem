9401e5da9fa3bdb068681f2e379b2831c323fee5
Nanmaran Ramesh <nmaran294@gmail.com>
2026-01-27 13:58:08 +0530

    Add files via upload
    pattern 8 in C

---
+++ problem8.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int rows = 5; 
+    for(int i=rows; i>=1; i--){
+        for(int k=1;k<=rows-i;k++){
+            printf(" ");
+        }
+        for(int j=(2*i-1);j>=1;j--){
+            printf("*");
+        }
+        
+        printf("\n");
+    }
+    return 0;
+}
